c5eb2786bc48702e091c1abfff2aa8de9c67fff7
Rajaneesh <65807307+Rajaneesh98@users.noreply.github.com>
2022-09-04 17:18:05 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Is-your-birthday-lucky-MarkEleven
-Created with CodeSandbox
+# Is Your Birthday Lucky
+
+Takes Input your Birthday and your lucky number
+
+Performs some operations on data to state whether your birthday is lucky or not.
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  var [date, updateDate] = useState("");
+  var [luckyNumber, updateLuckyNumber] = useState("");
+  var [showMsg, updateMsg] = useState("");
+  var sum = 0;
+
+  function takeDate(event) {
+    let date = event.target.value;
+
+    let updatedDate = date.replaceAll("-", "");
+    updateDate(updatedDate);
+  }
+
+  function takeNumber(event) {
+    var luckynum = event.target.value;
+
+    updateLuckyNumber(luckynum);
+  }
+
+  function checkForLucky() {
+    if (date === "" || luckyNumber === "") {
+      updateMsg(" please give your details");
+    } else {
+      for (var item of date) {
+        sum = sum + parseInt(item);
+      }
+      if (sum % luckyNumber === 0) {
+        updateMsg("yes you are lucky");
+      } else {
+        updateMsg("you are not lucky");
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <h2>Is Your Birthday Lucky?</h2>
+      <label>Date Of Birth:</label>
+      <input type="date" onChange={takeDate} />
+      <label>Enter your lucky number:</label>
+      <input type="number" onChange={takeNumber} />
+      <button onClick={checkForLucky}>Check Now</button>
+      <h2>{showMsg}</h2>
+      <footer>
+        <a href="https://github.com/Rajaneesh98">GitHub</a>
+        <a href="https://twitter.com/EgRajaneesh">Twitter</a>
+      </footer>
+    </div>
+  );
+}
